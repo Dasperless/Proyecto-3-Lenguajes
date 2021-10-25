82b595a75fc73d82900ec519b44dc9b747c532e5
Darío Vargas <dariovm13@gmail.com>
2021-10-24 22:32:58 -0600

    [ADD]: Creación de tablero por prolog

--- Programa/sudokux/src/sudokux/SudokuXController.java
+++ Programa/sudokux/src/sudokux/SudokuXController.java
@@ -1,6 +1,6 @@
 package sudokux;
 
-import org.jpl7.Atom;
+import java.util.Random;
 import org.jpl7.Query;
 import org.jpl7.Term;
 /*
@@ -14,13 +14,42 @@ import org.jpl7.Term;
  */
 public class SudokuXController {
     private String[] board;
-    public SudokuXController(){}
+    
+    
+    public SudokuXController(){
+        //Carga el archivo del juego.
+        Query load = new Query("consult(game)");
+        load.hasSolution();        
+    }    
+
+    public static void main(String[] args) {
+        SudokuXController x = new SudokuXController();
+        System.out.println(x.randomNum());
+//        x.genPrologBoard();
+    }    
     
     public String[] getBoard(){return board;}
+    
     public void setBoard(String[] pBoard){board=pBoard;}
-    public void genPrologBoard(){
-        Query q1 = new Query("consult",new Term[]{new Atom("game.pl")});
-        System.out.println("consult "+(q1.hasSolution()?"succed":"failed"));        
+    
+    private long randomNum(){
+        long leftLimit = 1;
+        long rightLimit = 10000;
+        long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
+        return generatedLong;
     }
     
+    public void genPrologBoard(){
+        String queryBoardStr= "getBoard(Row)";
+        Query queryBoard = new Query(queryBoardStr);
+
+        java.util.Map<String,Term> solution;
+        solution = queryBoard.oneSolution();
+        long randomNum = randomNum();
+        for(long i = 0; i<randomNum;i++){
+            solution = queryBoard.nextSolution(); 
+        }
+        System.out.println("X=" + solution.get("Row"));
+    }
 }
+
