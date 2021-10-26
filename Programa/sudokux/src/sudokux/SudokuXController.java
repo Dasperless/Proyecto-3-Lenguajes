76563cce3a0a3eed8223f19b986860e024592717
Darío Vargas <dariovm13@gmail.com>
2021-10-25 23:29:30 -0600

    [ADD]: Creación del tablero

--- Programa/sudokux/src/sudokux/SudokuXController.java
+++ Programa/sudokux/src/sudokux/SudokuXController.java
@@ -1,6 +1,7 @@
 package sudokux;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.jpl7.Query;
 import org.jpl7.Term;
 /*
@@ -24,32 +25,59 @@ public class SudokuXController {
 
     public static void main(String[] args) {
         SudokuXController x = new SudokuXController();
-        System.out.println(x.randomNum());
-//        x.genPrologBoard();
+        x.genPrologBoard();
     }    
     
     public String[] getBoard(){return board;}
     
     public void setBoard(String[] pBoard){board=pBoard;}
     
-    private long randomNum(){
-        long leftLimit = 1;
-        long rightLimit = 10000;
-        long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
-        return generatedLong;
+    /**
+     * Genera un número aleatorio entero
+     * @param leftLimit
+     * @param rightLimit
+     * @return un entero.
+     */
+    private int randInt(int leftLimit,int rightLimit){
+        int randomNum = leftLimit + (int) (Math.random() * (rightLimit - leftLimit));
+        return randomNum;
     }
     
-    public void genPrologBoard(){
-        String queryBoardStr= "getBoard(Row)";
+    /**
+     * Genera una matriz n cantidad de números aleatorios.
+     * @return 
+     */
+    private String boardSeed(){
+        ArrayList<ArrayList<String>> emptyBoard =  new ArrayList<>();
+        
+        //Genera el tablero vacío
+        for (int i = 0; i <9;i++){
+            ArrayList<String> row =  new ArrayList<>(Arrays.asList("_","_","_","_","_","_","_","_","_"));
+            emptyBoard.add(row);
+        }
+        
+        //Ingresa números aleatorios.
+        for(int i = 0; i < 5;i++){
+            int indexI = randInt(0,8);
+            int indexJ = randInt(0,8);
+            int num = randInt(1,9);
+            emptyBoard.get(indexI).set(indexJ,String.valueOf(num));
+        }
+        return emptyBoard.toString();
+    }
+    
+    /**
+     * Genera un tablero de sudoku en prolog.
+     * @return 
+     */
+    public String genPrologBoard(){
+        String seed = boardSeed(); //Semilla para general el tablero
+        String queryBoardStr= String.format("getBoard(%s,Board)", seed);
         Query queryBoard = new Query(queryBoardStr);
 
         java.util.Map<String,Term> solution;
         solution = queryBoard.oneSolution();
-        long randomNum = randomNum();
-        for(long i = 0; i<randomNum;i++){
-            solution = queryBoard.nextSolution(); 
-        }
-        System.out.println("X=" + solution.get("Row"));
+
+        return solution.get("Board").toString();
     }
 }
-
